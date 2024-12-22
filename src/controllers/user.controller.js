074742cdd10b223dbc06942c6851e4b955d669ab
Taim-Amer <taim.amer.000@gmail.com>
2024-12-22 15:44:12 -0800

    Delete user account controller

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,8 +1,8 @@
 import User from "../models/user.model.js";
 import bcrypt, { hash } from "bcrypt";
 
-export const update = async (req, res) => {
-    if(req.body.userId === req.params.id || res.user.isAdmin){
+export const updateUser = async (req, res) => {
+    if(req.body.userId === req.params.id || req.body.isAdmin){
         if(req.body.password){
             try{
                 const salt = await bcrypt.genSalt(10)
@@ -23,3 +23,28 @@ export const update = async (req, res) => {
         return res.status(403).json("You can update only your account")
     }
 }
+
+export const deleteUser = async (req, res) => {
+    if(req.body.userId === req.params.id || req.body.isAdmin){
+        try{
+            const user = await User.findByIdAndDelete(req.params.id)
+            res.status(200).json("Account has been deleted")
+        } catch(error) {
+            return res.status(500).json(error.message)
+        }
+    } else{
+        return res.status(403).json("You can delete only your account")
+    }
+}
+
+export const getUser = async (req, res) => {
+    try{
+        const user = User.findById(req.params.id)
+        const { password, updatedAt, ...other} = user._doc
+        res.status(200).json(other)
+    } catch(error){
+        res.status(500).json(error)
+    }
+}
+
+export const followUser = a
--- src/routes/users.route.js
+++ src/routes/users.route.js
@@ -1,8 +1,11 @@
 import express from "express";
+import { updateUser, deleteUser, getUser } from "../controllers/user.controller";
 
 const router = express.Router()
 
-router.put("/:id", update)
+router.put("/:id", updateUser)
+router.delete("/:id", deleteUser)
+router.get("/:id", getUser)
 
 
 export default router;
