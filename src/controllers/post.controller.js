dad2d7e4603ea662cca6e9153f2b401173f14b28
Taim-Amer <taim.amer.000@gmail.com>
2024-12-23 01:09:25 -0800

    Like and dislike post

--- src/controllers/post.controller.js
+++ src/controllers/post.controller.js
@@ -35,3 +35,18 @@ export const deletePost = async (req, res) => {
         res.status(500).json(error.message)
     }
 }
+
+export const likePost = async (req, res) => {
+    try{    
+        const post = await Post.findById(req.params.id)
+        if(!post.likes.includes(req.body.userId)){
+            await post.updateOne({ $push: {likes: req.body.userId} })
+            res.status(200).json("The post has been liked")
+        } else{
+            await post.updateOne({ $pull: {likes: req.body.userId} })
+            res.status(200).json("The post has been disliked")
+        }
+    } catch(error){
+        res.status(500).json(error.message)
+    }
+}
--- src/routes/posts.route.js
+++ src/routes/posts.route.js
@@ -1,9 +1,11 @@
 import express from "express";
-import { createPost, updatePost } from "../controllers/post.controller.js";
+import { createPost, deletePost, updatePost, likePost } from "../controllers/post.controller.js";
 
 const router = express.Router()
 
 router.post("/", createPost)
 router.put("/:id", updatePost)
+router.delete("/:id", deletePost)
+router.put("/:id/like", likePost)
 
 export default router;
