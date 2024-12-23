f06ff3d3309f238bc751b0319631a3145f85889d
Taim-Amer <taim.amer.000@gmail.com>
2024-12-22 16:09:10 -0800

    Follow user

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -47,4 +47,23 @@ export const getUser = async (req, res) => {
     }
 }
 
-export const followUser = a
+export const followUser = async (req, res) => {
+    if(req.body.userId !== req.params.id){
+        try{
+            const user = await User.findById(req.params.id)
+            const currentUser = await User.findById(req.body.userId)
+            if(!user.followers.includes(req.body.userId)){
+                await user.updateOne({ $push: { followers: req.body.userId }})
+                await currentUser.updateOne({ $push: { followings: req.params.id }})
+                res.status(200).json("User has been followed")
+
+            } else{
+                res.status(403).json("You already follow this user")
+            }
+        } catch(error){
+            res.status(500).json(error)
+        }   
+    } else{
+        res.status(403).json("You cant follow yourself")
+    }
+}   
--- src/routes/users.route.js
+++ src/routes/users.route.js
@@ -1,11 +1,12 @@
 import express from "express";
-import { updateUser, deleteUser, getUser } from "../controllers/user.controller";
+import { updateUser, deleteUser, getUser, followUser } from "../controllers/user.controller";
 
 const router = express.Router()
 
 router.put("/:id", updateUser)
 router.delete("/:id", deleteUser)
 router.get("/:id", getUser)
+router.put("/:id/follow", followUser)
 
 
 export default router;
